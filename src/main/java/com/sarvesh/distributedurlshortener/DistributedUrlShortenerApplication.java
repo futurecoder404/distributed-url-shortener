8b6c88ac23c39beee7e7e25923c67c684d736270
Sarvesh Bhushan <sarveshbhushan575@gmail.com>
2026-05-13 00:19:04 +0530

    Initial Spring Boot and PostgreSQL setup

---
+++ src/main/java/com/sarvesh/distributedurlshortener/DistributedUrlShortenerApplication.java
@@ -0,0 +1,12 @@
+package com.sarvesh.distributedurlshortener;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistributedUrlShortenerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DistributedUrlShortenerApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sarvesh/distributedurlshortener/DistributedUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sarvesh.distributedurlshortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DistributedUrlShortenerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
